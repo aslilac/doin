a8462707c12d87061756ec796d917581731db92c
Tynan Beatty <38031130+tynanbe@users.noreply.github.com>
2023-02-10 11:42:19 -0600

    Retain status code from subprocess (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 mod completions;
 
-use std::{env, io, process::Command};
+use std::{
+    env,
+    process::{Command, ExitCode},
+};
 
-fn main() -> io::Result<()> {
+fn main() -> ExitCode {
     let mut args = env::args().skip(1);
     let dir_or_flag = args.next().expect("Must provide a directory to run in!");
 
     if dir_or_flag == "--fish-completions" {
         println!("{}", completions::FISH);
-        return Ok(());
+        return ExitCode::SUCCESS;
     }
 
     let dir = dir_or_flag;
     let command = args.next().expect("Must provide a command to run!");
-    Command::new(command).current_dir(dir).args(args).status()?;
-    Ok(())
+    let status = Command::new(command)
+        .current_dir(dir)
+        .args(args)
+        .status()
+        .expect("Failed to execute the command!");
+    let code = status.code().expect("Process terminated by signal!");
+
+    ExitCode::from(code as u8)
 }
